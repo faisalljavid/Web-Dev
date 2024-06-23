f910eb771db4407a436b38bad839de46f8aeeee8
faisalljavid <faisalljavid@gmail.com>
2024-06-23 16:00:42 +0530

    revise callback functions
    - adding example to callback function topic

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -4,12 +4,34 @@ function calculateArithmetic(a, b, arithmeticFunction) {
 }
 
 function sum(a, b) {
-    return a+b;
+    return a + b;
 }
 
 function sub(a, b) {
-    return a-b;
+    return a - b;
 }
 
 const value = calculateArithmetic(1, 2, sub);
 console.log(value);
+
+
+
+
+
+// one more example
+function square(n) {
+    return n * n;
+}
+
+function cube(n) {
+    return n * n * n;
+}
+
+function sumOfSomething(a, b, callback) {
+    const val1 = callback(a);
+    const val2 = callback(b);
+
+    return val1 + val2;
+}
+
+console.log(sumOfSomething(2, 2, cube));
